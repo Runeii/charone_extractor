72ea5ebaccbe34d47a9d36d2b577ad2b1bf52e28
Andrew Hill <contact@andrewthomashill.co.uk>
2025-01-20 15:45:53 +0100

    Support main character textures

--- src/parse/mch/mch_header.py
+++ src/parse/mch/mch_header.py
@@ -1,9 +1,11 @@
 from io import BytesIO
 from ...utils.binary_reader import BinaryReader
+from dataclasses import dataclass, field
 
+@dataclass(init=False)
 class MCHHeader:
-  tim_offsets: list[int] = []
-  model_data_offset: int
+  tim_offsets: list[int]
+  model_data_offset: int = field(init=False)
 
   def __init__(self, data: bytes):
     if len(data) != 256:
@@ -11,11 +13,13 @@ class MCHHeader:
     
     stream = BytesIO(data)
 
+    self.tim_offsets = []
+
     while True:
       offset = BinaryReader.read_uint32(stream)
-      if offset == 4294967295:
+      if offset == 4294967295: 
         break
-       
+      
       self.tim_offsets.append(offset)
     
     self.model_data_offset = BinaryReader.read_uint32(stream)
--- src/parse/model/__parser.py
+++ src/parse/model/__parser.py
@@ -3,22 +3,23 @@ from .tim import TIM
 from .model_data import ModelData
 from ..headers.model_header import ModelHeader
 from ..mch.mch_header import MCHHeader
+from typing import List
 
 @dataclass
 class ModelParser:
   header: ModelHeader
   model_data: ModelData = field(init=False)
-  tim: TIM = field(init=False)
+  textures: List[TIM] = field(init=False)
 
   def __init__(self, header: ModelHeader, data: bytes):
     self.header = header
 
     if header.is_main_field_model:
-      self.init_main_character_model(name=header.model_name, char_one_data=data[header.model_offset + 4:])
+      self.parse_main_character_model(name=header.model_name, char_one_data=data[header.model_offset + 4:])
     else:
-      self.init_non_main_character_model(data)
+      self.parse_non_main_character_model(data)
 
-  def init_main_character_model(self, name: str, char_one_data: bytes):
+  def parse_main_character_model(self, name: str, char_one_data: bytes):
     sanitised_name = name.replace("x", "")
     with open(f"./INPUT/models/{sanitised_name}.mch", "rb") as f:
       data = f.read()
@@ -29,22 +30,36 @@ class ModelParser:
       data=data,
       name=self.header.model_name,
       offset=mch_header.model_data_offset,
-      char_one_data=char_one_data,
+      char_one_data=char_one_data, # wiki: should note animation data location
     )
-    pass
 
-  def init_non_main_character_model(self, data: bytes):
+    self.textures = self.parse_textures(data, mch_header.tim_offsets)
+
+  def parse_non_main_character_model(self, data: bytes):
     self.model_data = ModelData(
       data=data,
       name=self.header.model_name,
       offset=self.header.model_offset + self.header.model_data_offset + 4,
     )
-
-    tim_offset = self.header.tim_offsets[0]
     
     # Note: need to +4 for PC files
-    self.tim = TIM(
-      name=self.header.model_name,
-      data=data[self.header.model_offset + tim_offset + 4:]
-    )
+    tim_offsets = list(
+      map(
+        lambda tim_offset: self.header.model_offset + tim_offset + 4, self.header.tim_offsets
+        )
+    );
+    
+    self.textures = self.parse_textures(data, tim_offsets)
 
+  def parse_textures(self, data: bytes, offsets: List[int]):
+    textures: List[TIM] = []
+
+    for offset in offsets:
+      tim = TIM(
+        name=self.header.model_name,
+        data=data[offset:]
+      )
+
+      textures.append(tim)
+    
+    return textures
