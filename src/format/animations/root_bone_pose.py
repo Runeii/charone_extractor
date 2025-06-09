ab90033732f8e9bf45d0a1e0348f769a50ed155a
Andrew Hill <contact@andrewthomashill.co.uk>
2025-06-09 08:22:57 +0200

    Handle offset

--- src/format/animations/root_bone_pose.py
+++ src/format/animations/root_bone_pose.py
@@ -13,7 +13,21 @@ class RootBonePose(BonePose):
     super().__init__(original=pose)
     
     self.location = [
-        offset[0] / 256,
-        offset[1] / 256,
-        offset[2] / 256,
+        self.scale_offset(
+          self.handle_negative_bone_length(offset[0])
+        ),
+        self.scale_offset(
+          self.handle_negative_bone_length(offset[1])
+        ),
+        self.scale_offset(
+          self.handle_negative_bone_length(offset[2])
+        ),
     ]
+  
+  def handle_negative_bone_length(self, bone_length: int):
+    if bone_length > 32768:
+        return bone_length - 65536
+    return bone_length
+  
+  def scale_offset(self, offset: int):
+    return offset / 256
