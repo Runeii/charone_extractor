8814fda40f44d809117b1eda1a3550efb46982d2
Andrew Hill <contact@andrewthomashill.co.uk>
2025-08-10 20:27:44 +0200

    Export multiple meshes

--- src/main.py
+++ src/main.py
@@ -173,8 +173,8 @@ def process_file(filepath: str) -> None:
         model_name = model_header.model_name
         print(f"Processing model {model_name}")
 
-        #if model_name != 'd049' and model_name != 'p017' and model_name != 'd001' and model_name != 'p001':
-        #  continue
+        if not model_name.startswith('n'):
+          continue
         # Check if a file with model name exists in the same folder
         mch_model_file_path = os.path.join(file_directory, f"{model_name}.mch")
         if (model_name.startswith('d') == True) and (os.path.exists(mch_model_file_path) == False):
